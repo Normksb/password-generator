4ad8c0d72dd77bfaaa747a89f81b3d081cfbce40
Norman Bernard <nksb414@gmail.com>
2021-03-27 12:15:41 +0800

    array low to high iteration function added and ASCII code variables created

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// create a function to iterate through ASCII decimal values and store those in an array called "array".
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+      array.push(i)
+  }
+  return array
+}
+
+// Invoke the function arrayFromLowToHigh. Provide parameters to the function that give the ranges of ASCII decimal values to retreive for each variable
+var ASCIILowerCaseChars = arrayFromLowToHigh(97,122);
+var ASCIIUpperCaseChars = arrayFromLowToHigh(65,90);
+var ASCIINumberChars = arrayFromLowToHigh(48,57);
+var ASCIISymbolChars = arrayFromLowToHigh(33, 47)
+   .concat(arrayFromLowToHigh(58, 64))
+   .concat(arrayFromLowToHigh(91, 96))
+   .concat(arrayFromLowToHigh(123, 126));
+
+// Create empty array. The user input from prompts will be stored here before generating random password.
+var ASCIIToUseInPassword = [];
